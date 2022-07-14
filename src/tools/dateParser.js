acb7b4285865fd9b8fa9e397b60ecdeaa1aa61df
gissehel <public-devgit-common@gissehel.org>
2022-07-14 16:53:50 +0200

    TEST : parser first test set (404 tests)

---
+++ src/tools/dateParser.js
@@ -0,0 +1,7 @@
+export const FORMAT_UNKNOWN='UNKNOWN'
+export const FORMAT_GREGORIAN='GREGORIAN'
+export const FORMAT_FRENCH_REPUBLICAN='FRENCH_REPUBLICAN'
+
+export const parseDate = (data) => {
+    return {};
+}
